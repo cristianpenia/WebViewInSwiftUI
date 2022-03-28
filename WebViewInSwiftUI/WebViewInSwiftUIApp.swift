1999363bd60b5f978b02750a31b4d60ba8791ee6
Cristian_Mac2015 <cris_pb@protonmail.com>
2022-03-28 09:10:08 -0600

    Initial Commit

---
+++ WebViewInSwiftUI/WebViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewInSwiftUIApp.swift
+//  WebViewInSwiftUI
+//
+//  Created by Cristian Peña Barrios on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
